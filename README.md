9a2ad9de927fc5ba508e18d13cd52ad4e3f0ee3c
danilo.oliveira <danilo.oliveira@prestadores.intermedica.com.br>
2023-04-18 18:16:11 -0300

    Envio de arquivos

---
+++ README.md
@@ -0,0 +1,88 @@
+# PDV - Gestão de Ponto de Vendas
+
+Sistema PDV feito em Laravel para gestão de vendas.
+
+## Caracteríticas
+
+- OAuth
+- Login
+- Rotas
+- CRUD
+- DataTable
+- Filtro
+- Paginador
+- View Blade
+- Responsivo
+
+## Requisitos
+
+- PHP >= 7.4.33
+- MySql >=5
+- Composer
+
+## Tecnologia
+
+- PHP
+- JS
+- Jquery
+- Json
+- CSS and SCSS
+- Bootstrap
+- Composer
+- Artisan
+- Laravel
+
+## Instalação
+
+Criar Banco de Dados Mysql com nome 'pdv':
+
+```
+$ mysql -u <your user> -p <your password>
+
+mysql> create database `pdv`;
+```
+OBS: Editar o arquivo .env conforme os dados de conexão do banco de dados.<br>
+Após configurar o banco, rodar o seguinte comando:
+
+```
+$ composer install
+
+$ php artisan key:generate
+
+$ php artisan migrate
+
+$ php artisan db:seed
+
+$ php artisan serve
+```
+
+## Acesso ao Sistema 
+
+Para acessar o sistema utilize o seguinte endereço:<br>
+http://localhost:8000/
+
+- Login: <br>
+    - Usuário: admin<br>
+    - Senha: admin
+
+## Licença
+
+Projeto licenciado sob <a href="LICENSE">The MIT License (MIT)</a>.
+
+## Screenshots
+
+![Screenshots](screenshots/screenshot01.png) <br><br>
+![Screenshots](screenshots/screenshot02.png) <br><br>
+![Screenshots](screenshots/screenshot03.png) <br><br>
+![Screenshots](screenshots/screenshot04.png) <br><br>
+![Screenshots](screenshots/screenshot05.png) <br><br>
+![Screenshots](screenshots/screenshot06.png) <br><br>
+![Screenshots](screenshots/screenshot07.png) <br><br>
+![Screenshots](screenshots/screenshot08.png) <br><br>
+![Screenshots](screenshots/screenshot09.png) <br><br>
+
+
+Desenvolvido por<br>
+Danilo Meneghel<br>
+danilo.meneghel@gmail.com<br>
+http://danilomeneghel.github.io/<br>
---
+++ resources/views/vendor/adminlte/login.blade.php
@@ -0,0 +1,86 @@
+@extends('adminlte::master')
+
+@section('adminlte_css')
+    <link rel="stylesheet" href="{{ asset('vendor/adminlte/plugins/iCheck/square/red.css') }}">
+    <link rel="stylesheet" href="{{ asset('vendor/adminlte/css/auth.css') }}">
+    @yield('css')
+@stop
+
+@section('body_class', 'login-page')
+
+@section('body')
+    <div class="login-box">
+        <div class="logo-login">
+          <img src="{{ asset('images/logo.png') }}" width="345" height="160">
+        </div>
+        <div class="login-box-body">
+            <p class="login-box-msg">Gestão de Ponto de Venda</p>
+            <form action="{{ url(config('adminlte.login_url', 'login')) }}" method="post">
+                {!! csrf_field() !!}
+
+                <div class="form-group has-feedback {{ $errors->has('username') ? 'has-error' : '' }}">
+                    <input type="text" name="username" class="form-control" value="{{ old('username') }}"
+                           placeholder="Usuário">
+                    <span class="glyphicon glyphicon-user form-control-feedback"></span>
+                    @if ($errors->has('username'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('username') }}</strong>
+                        </span>
+                    @endif
+                </div>
+                <div class="form-group has-feedback {{ $errors->has('password') ? 'has-error' : '' }}">
+                    <input type="password" name="password" class="form-control"
+                           placeholder="Senha">
+                    <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+                    @if ($errors->has('password'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('password') }}</strong>
+                        </span>
+                    @endif
+                </div>
+                <div class="row">
+                    <!-- <div class="col-xs-8">
+                        <div class="checkbox icheck">
+                            <label>
+                                <input type="checkbox" name="remember"> {{ trans('adminlte::adminlte.remember_me') }}
+                            </label>
+                        </div>
+                    </div> -->
+                    <!-- /.col -->
+                    <div class="col-xs-12">
+                        <button type="submit"
+                                class="btn btn-primary btn-block btn-flat">Entrar</button>
+                    </div>
+                    <!-- /.col -->
+                </div>
+            </form>
+            <div class="row" style="padding:8px 0px">
+              <div class="col-xs-6">
+                <div class="pull-left">
+                  <a ui-sref="forgot_password" href="{{ route('password.request') }}">Esqueceu sua senha?</a>
+                </div>
+              </div>
+              <div class="col-xs-6">
+                <div class="pull-right">
+                  <a ui-sref="register" class="text-center" href="{{ route('register') }}">Criar uma conta</a>
+                </div>
+              </div>
+            </div>
+        </div>
+        <!-- /.login-box-body -->
+    </div><!-- /.login-box -->
+@stop
+
+@section('adminlte_js')
+    <script src="{{ asset('vendor/adminlte/plugins/iCheck/icheck.min.js') }}"></script>
+    <script>
+        $(function () {
+            $('input').iCheck({
+                checkboxClass: 'icheckbox_square-blue',
+                radioClass: 'iradio_square-blue',
+                increaseArea: '20%' // optional
+            });
+        });
+    </script>
+    @yield('js')
+@stop
