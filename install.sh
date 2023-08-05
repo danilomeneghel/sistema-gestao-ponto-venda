50b4d4cb54a4b3ea5172431cb64c2b7428648a09
Danilo Meneghel <danilo.meneghel@gmail.com>
2023-08-05 18:28:13 -0300

    Atualização de arquivos

--- install.sh
+++ install.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
-sleep 12
+sleep 20
 composer update
 composer install
 php artisan optimize:clear
 php artisan key:generate
-php artisan clear-compiled
-php artisan cache:clear
-php artisan config:clear
-php artisan route:clear
-php artisan package:discover
 php artisan migrate
 php artisan db:seed
 php artisan serve
