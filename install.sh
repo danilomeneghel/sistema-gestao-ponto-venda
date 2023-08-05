d73b27c340fdd62464e79d9a00aa3ecc6389e864
Danilo Meneghel <danilo.meneghel@gmail.com>
2023-08-04 22:55:26 -0300

    Atualização de arquivos

--- install.sh
+++ install.sh
@@ -6,4 +6,4 @@ php artisan optimize:clear
 php artisan key:generate
 php artisan migrate
 php artisan db:seed
-php artisan serve --host=0.0.0.0 --port=80
+php artisan serve --host=https://gestao-ponto-venda.up.railway.app --port=80
