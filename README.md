a8197e2deb03fd9aef1b57090fefefa91e7c8f58
Danilo Meneghel <danilo.meneghel@gmail.com>
2023-08-06 01:11:39 -0300

    Atualização de arquivos

--- README.md
+++ README.md
@@ -77,13 +77,13 @@ $ php artisan db:seed
 Depois de realizado isso, rode o projeto:
 
 ```
-$ php artisan serve --host=0.0.0.0 --port=80
+$ php artisan serve --host=0.0.0.0 --port=8000
 ```
 
 ## Acesso ao Sistema 
 
 Para acessar o sistema utilize o seguinte endereço:<br>
-http://localhost
+http://localhost:8000
 
 - Login: <br>
     - Usuário: admin<br>
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,23 +2,12 @@
 
 namespace App\Models;
 
-<<<<<<< HEAD
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
     use Notifiable;
-=======
-use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-
-class User extends Authenticatable
-{
-    use HasFactory, Notifiable;
->>>>>>> e8ce190fbe96afcfe33330b5fde3cf1a694486c0
 
     /**
      * The attributes that are mass assignable.
@@ -26,13 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-<<<<<<< HEAD
         'name', 'username', 'email', 'password',
-=======
-        'name',
-        'email',
-        'password',
->>>>>>> e8ce190fbe96afcfe33330b5fde3cf1a694486c0
     ];
 
     /**
@@ -41,20 +24,6 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-<<<<<<< HEAD
         'password', 'remember_token',
-=======
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
->>>>>>> e8ce190fbe96afcfe33330b5fde3cf1a694486c0
     ];
 }
